b85f2bb7f34e50a2d0a275a92a3d90231f68a8ec
pattiemecon <patoattie@gmail.com>
2016-11-15 19:54:19 -0300

    20161115_03

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -4,13 +4,43 @@ B.	Mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un ter
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
 
+
 function Rectangulo () 
 {
+	var largoTerreno;
+	var anchoTerreno;
+	var cantidadAlambre;
+
+	largoTerreno=document.getElementById('Largo').value;
+	anchoTerreno=document.getElementById('Ancho').value;
+
+	cantidadAlambre=parseInt(largoTerreno)+parseInt(anchoTerreno);
+	// calculo el perimetro del rectangulo
+	cantidadAlambre=cantidadAlambre*2;
+	// calculo la cantidad de alambre (3 vueltas)
+	cantidadAlambre=cantidadAlambre*3;
+
+	alert("La cantidad de alambre a comprar es "+cantidadAlambre+" m.");
 
 }
 function Circulo () 
 {
-	
+	var radioTerreno;
+	var cantidadAlambre;
+
+	radioTerreno=document.getElementById('Radio').value;
+
+	// calculo el perimetro del circulo
+	cantidadAlambre=Math.PI*radioTerreno*2;
+	// calculo la cantidad de alambre
+	cantidadAlambre=cantidadAlambre*3;
+
+	// debugging
+	/*console.log("PI: "+Math.PI);
+	console.log("PI*radioTerreno: "+(Math.PI*radioTerreno));
+	console.log("radioTerreno*2: "+(2*radioTerreno));*/
+
+	alert("La cantidad de alambre a comprar es "+cantidadAlambre+" m.");
 }
 function Materiales () 
 {
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,49 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+
+// declaracion de variables
+var precioUno;
+var precioDos;
+var precioTres;
+var resultado;
+
+// console.log() -> Muestra mensajes en la consola
+
 function Sumar () 
 {
-	
+	precioUno=document.getElementById('PrecioUno').value;
+	precioDos=document.getElementById('PrecioDos').value;
+	precioTres=document.getElementById('PrecioTres').value;
+
+	//prueba console.log
+	console.log("HOLA");
+	console.log(precioUno);
+
+	resultado=parseInt(precioUno)+parseInt(precioDos)+parseInt(precioTres);
+
+	alert("La suma de los precios es "+resultado);
+
 }
 function Promedio () 
 {
+	precioUno=document.getElementById('PrecioUno').value;
+	precioDos=document.getElementById('PrecioDos').value;
+	precioTres=document.getElementById('PrecioTres').value;
+
+	resultado=(parseInt(precioUno)+parseInt(precioDos)+parseInt(precioTres))/3;
+
+	alert("El promedio de los precios es "+resultado);
 	
 }
 function PrecioFinal () 
 {
+	precioUno=document.getElementById('PrecioUno').value;
+	precioDos=document.getElementById('PrecioDos').value;
+	precioTres=document.getElementById('PrecioTres').value;
+
+	resultado=(parseInt(precioUno)+parseInt(precioDos)+parseInt(precioTres))*1.21;
+
+	alert("La suma de los precios más IVA es "+resultado);
 	
 }
