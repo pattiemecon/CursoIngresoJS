efb0a290c3b262ba941af87964c40274b3ecd87f
pattiemecon <patoattie@gmail.com>
2016-11-15 18:53:44 -0300

    20161115_02

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -8,8 +8,13 @@ function MostrarAumento()
 	var resultado;
 
 	sueldo=document.getElementById('sueldo').value;
-	resultado=sueldo*1.1;
+	// opc mia
+	/*resultado=sueldo*1.1;
 
-	document.getElementById('resultado').value=resultado;
+	document.getElementById('resultado').value=resultado;*/
+
+	// opc profe
+	resultado=parseInt(sueldo)+(sueldo/100*10);
+	document.getElementById('resultado').value="Su sueldo se aumento a "+resultado;
 	
 }
