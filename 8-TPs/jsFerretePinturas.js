8089e4a1e2820e8fdd5cd4892c2c7907989cdcea
pattiemecon <patoattie@gmail.com>
2016-11-20 15:51:10 -0300

    20161120_01_casa

--- 8-TPs/jsFerretePinturas.js
+++ 8-TPs/jsFerretePinturas.js
@@ -4,10 +4,32 @@ B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en
 */
 function FahrenheitCentigrados () 
 {
-	
+	//Fahrenheit a Celsius	(°F - 32) x 100/180 = °C
+
+	var temperaturaFarenheit;
+	var temperaturaCentigrados;
+
+	temperaturaFarenheit=document.getElementById('Temperatura').value; //obtengo el valor ingresado por el usuario
+
+	temperaturaCentigrados=temperaturaFarenheit-32; //resuelvo el primer termino de la formula
+	temperaturaCentigrados=temperaturaCentigrados*100/180; //termino de resolver la formula.
+
+	alert(temperaturaFarenheit+" Fahrenheit son "+temperaturaCentigrados+" centígrados"); //muestro el resultado al usuario.
+
 }
 
 function CentigradosFahrenheit () 
 {
+	//Celsius a Fahrenheit	(°C × 180/100) + 32 = °F
+
+	var temperaturaFarenheit;
+	var temperaturaCentigrados;
+
+	temperaturaCentigrados=document.getElementById('Temperatura').value; //obtengo el valor ingresado por el usuario
+
+	temperaturaFarenheit=temperaturaCentigrados*180/100; //resuelvo el primer termino de la formula
+	temperaturaFarenheit=temperaturaFarenheit+32; //termino de resolver la formula.
+
+	alert(temperaturaCentigrados+" centígrados son "+temperaturaFarenheit+" Fahrenheit"); //muestro el resultado al usuario.
 	
 }
