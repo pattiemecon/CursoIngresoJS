c72daed8a6bb133e0f2f54e0d9b0cff81a014c7d
pattiemecon <patoattie@gmail.com>
2016-12-10 09:22:10 -0300

    20161209_01_casa

--- 8-TPs/jsReflejosDaltonicos1.js
+++ 8-TPs/jsReflejosDaltonicos1.js
@@ -6,16 +6,55 @@ mostrara el texto de un color entre los 6 posibles
 */
 var ColorSecreto;
 var tiempoInicio;
+var tiempoTranscurrido=0;
+tiempoInicio=setInterval(ContarTiempo,1000);
+
 function comenzar()
 {
 
-	
+	//Genero el número RANDOM entre 1 y 6
+ 	ColorSecreto=Math.floor(Math.random()*6)+1;
+	//alert(ColorSecreto);
+	switch(ColorSecreto)
+	{
+		case 1:
+			ColorSecreto="azul";
+			break;
+		case 2:
+			ColorSecreto="amarillo";
+			break;
+		case 3:
+			ColorSecreto="marron";
+			break;
+		case 4:
+			ColorSecreto="verde";
+			break;
+		case 5:
+			ColorSecreto="celeste";
+			break;
+		case 6:
+			ColorSecreto="rojo";
+	}
+
+	document.getElementById('ColorElejido').value=ColorSecreto;
+
 
 }//FIN DE LA FUNCIÓN
 function Responder(colorParametro)
 {
 	
-	
+	if(colorParametro==ColorSecreto)
+	{
+		clearTimeout(tiempoInicio);
+		alert("Tardo "+tiempoTranscurrido+" segundos");
+	}
 
+
+}//FIN DE LA FUNCIÓN
+
+function ContarTiempo()
+{
+
+	tiempoTranscurrido++;
 
 }//FIN DE LA FUNCIÓN
